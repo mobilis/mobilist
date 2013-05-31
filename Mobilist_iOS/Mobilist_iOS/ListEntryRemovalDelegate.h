50f00783da93cd33b5fad3ee63490a21d58d5775
gannimet <r.wotzlaw@gmail.com>
2013-05-31 10:01:55 +0200

    complete msdl definition for Mobilist

---
+++ Mobilist_iOS/Mobilist_iOS/ListEntryRemovalDelegate.h
@@ -0,0 +1,13 @@
+//
+//  ListEntryRemovalDelegate.h
+//  Mobilist_iOS
+//
+//  Created by Richard Wotzlaw on 28.05.13.
+//  Copyright (c) 2013 TU Dresden. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ListEntryRemovalDelegate : NSObject
+
+@end
---
+++ Mobilist_iOS/Mobilist_iOS/ListEntryRemovalDelegate.m
@@ -0,0 +1,13 @@
+//
+//  ListEntryRemovalDelegate.m
+//  Mobilist_iOS
+//
+//  Created by Richard Wotzlaw on 28.05.13.
+//  Copyright (c) 2013 TU Dresden. All rights reserved.
+//
+
+#import "ListEntryRemovalDelegate.h"
+
+@implementation ListEntryRemovalDelegate
+
+@end
