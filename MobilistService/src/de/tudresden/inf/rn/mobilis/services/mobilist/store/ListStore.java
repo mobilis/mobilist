6ee3dffefd7595f6a7e54ccde561810e5df24a51
gannimet <r.wotzlaw@gmail.com>
2013-06-01 22:31:07 +0200

    Using dummy data to provide responses

---
+++ MobilistService/src/de/tudresden/inf/rn/mobilis/services/mobilist/store/ListStore.java
@@ -0,0 +1,69 @@
+package de.tudresden.inf.rn.mobilis.services.mobilist.store;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.tudresden.inf.rn.mobilis.services.mobilist.proxy.MobiList;
+
+public class ListStore {
+
+	/*
+	 * Singleton
+	 */
+	private ListStore() {}
+	
+	private static ListStore instance;
+	
+	public static synchronized ListStore getInstance() {
+		if (instance == null) {
+			instance = new ListStore();
+		}
+		
+		return instance;
+	}
+	/*
+	 * End singleton
+	 */
+	
+	private List<MobiList> allLists = new ArrayList<MobiList>();
+	
+	public List<MobiList> getAllLists() {
+		return allLists;
+	}
+
+	public void setAllLists(List<MobiList> allLists) {
+		this.allLists = allLists;
+	}
+	
+	public boolean addList(MobiList list) {
+		return allLists.add(list);
+	}
+	
+	public boolean removeList(String id) {
+		for (MobiList list : allLists) {
+			if (list.getId().equals(id)) {
+				return allLists.remove(list);
+			}
+		}
+		
+		return false;
+	}
+	
+	public MobiList getListById(String listId) {
+		for (MobiList list : allLists) {
+			if (listId.equals(list))
+				return list;
+		}
+		
+		return null;
+	}
+	
+	public void persist() {
+		
+	}
+	
+	public void load() {
+		
+	}
+	
+}
